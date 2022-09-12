f7df8965580cb5dedd490e751b46820b37c67acb
Carl Bednorz <carl@surfgreen.dev>
2022-09-12 16:25:38 +0200

    Configured posts app

--- self_service_portal_j/posts/apps.py
+++ self_service_portal_j/posts/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "self_service_portal_j.posts"
+    verbose_name = "Johanniter Posts"

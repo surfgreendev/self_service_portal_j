209aa5785088adcaa9be0e58cd7d6f5aad891545
Carl Bednorz <carl@surfgreen.dev>
2022-09-13 11:30:41 +0200

    Configured admin for post app

--- self_service_portal_j/posts/admin.py
+++ self_service_portal_j/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from self_service_portal_j.posts.models import Post, PostCategory
+
+
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ["slug", "published_on"]

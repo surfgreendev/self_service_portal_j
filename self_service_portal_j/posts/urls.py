a3760aee6a60cc6427cb594620461471d88c032f
Carl Bednorz <carl@surfgreen.dev>
2022-09-14 09:57:05 +0200

    Update

--- self_service_portal_j/posts/urls.py
+++ self_service_portal_j/posts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from self_service_portal_j.posts.views import PostCreateView
+from self_service_portal_j.posts.views import PostCategoryCreateView, PostCreateView
 
 app_name = "posts"
 
 urlpatterns = [
     path("create/", view=PostCreateView.as_view(), name="create"),
+    path(
+        "category/create/",
+        view=PostCategoryCreateView.as_view(),
+        name="category-create",
+    ),
 ]
--- self_service_portal_j/posts/views.py
+++ self_service_portal_j/posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 
-from self_service_portal_j.posts.models import Post
+from self_service_portal_j.posts.models import Post, PostCategory
 
 
 class PostCreateView(CreateView):
     model = Post
     fields = ["status", "title", "sub_title", "content", "tags"]
+
+
+class PostCategoryCreateView(CreateView):
+    model = PostCategory
+    fields = ["title", "description"]

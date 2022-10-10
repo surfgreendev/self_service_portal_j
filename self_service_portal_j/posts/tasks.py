2e539e2ee9774dde6926ecf74220ec3831a0eb6c
Carl Bednorz <carl@surfgreen.dev>
2022-10-10 12:59:15 +0200

    Sending mail with post list via task

--- self_service_portal_j/posts/tasks.py
+++ self_service_portal_j/posts/tasks.py
@@ -10,12 +10,14 @@ logger = get_task_logger(__name__)
 @celery_app.task()
 def send_list_of_blog_posts():
     posts = Post.objects.all()
+    post_string = ""
     for post in posts:
         logger.info("Blog Post %s" % post)
+        post_string += "Post: {} \n".format(post.title)
 
     send_mail(
         "List of posts",
-        "Here is the list of posts",
+        "Here is the list of posts\n " + post_string,
         "from@example.org",
         ["to@example.org"],
         fail_silently=False,
